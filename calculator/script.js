67407e835f813f3d8344c6d79eb3d77649277cd5
v4n <105587619+v4n00@users.noreply.github.com>
2024-10-15 12:04:48 +0300

    feat: clean starting point

--- calculator/script.js
+++ calculator/script.js
@@ -1,23 +1,14 @@
-for (let i = 9; i >= -1; i--) {
-	var btn = document.createElement('BUTTON');
-	btn.innerHTML = i;
-	if (i === 0) {
-		btn.setAttribute('id', 'zero');
+document.addEventListener('DOMContentLoaded', () => {
+	for (let i = 9; i >= -1; i--) {
+		var btn = document.createElement('button');
+		btn.innerHTML = i;
+		if (i === 0) {
+			btn.setAttribute('id', 'zero');
+		}
+		document.getElementById('digits').appendChild(btn);
+
+		btn.setAttribute('onClick', 'myFunction(this.innerHTML)');
 	}
+	btn.innerHTML = '.';
 	document.getElementById('cifre').appendChild(btn);
-
-	btn.setAttribute('onClick', 'myFunction(this.innerHTML)');
-}
-
-btn.innerHTML = '.';
-document.getElementById('cifre').appendChild(btn);
-
-function myFunction(clicked) {
-	let display = document.querySelector('#display p').innerHTML;
-	display = clicked;
-	if (isNaN(Number(clicked))) {
-		console.log('nu este cifra');
-	} else {
-		console.log(clicked);
-	}
-}
+});
